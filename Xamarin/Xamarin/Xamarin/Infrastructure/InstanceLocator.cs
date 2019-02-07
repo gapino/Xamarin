7e1aef4a1f061364638cf093b7745a0153abf53a
Guille <Guille@DESKTOP-P8R0CLN>
2019-02-07 14:47:15 -0300

    Arreglado Login

---
+++ Xamarin/Xamarin/Xamarin/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.ViewModels;
+
+namespace Xamarin.Infrastructure
+{
+    class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
